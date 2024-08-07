c97aae0fc90f8970624c34ed4780ff12094b937c
Dyjai White <175043959+DyjaiW@users.noreply.github.com>
2024-08-07 12:42:22 -1000

    Add project files.

---
+++ NewMortgageCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+
+namespace NewMortgageCalculator
+{
+    public class MortgageCalculator
+    {
+
+        public double MonthlyPayment(int loanAmount, float loanRate, int months)
+        {
+            double numerator = (loanAmount) * (loanRate / 1200);
+            double denominator = (1 - Math.Pow((1 + loanRate / 1200), -(months)));
+            return Math.Round((numerator / denominator), 2);
+        }
+
+        public double InterestPayment(float loanRate, float previousBalance) =>
+            Math.Round((previousBalance * loanRate / 1200), 2);
+
+
+        public double PrincipalPayment(float monthlyPayment, float interestPayment) =>
+           Math.Round((monthlyPayment - interestPayment), 2);
+
+
+        public double TotalLoan(float principalPayment, float interest)
+        {
+            return Math.Round((principalPayment + interest), 2);
+        }
+
+
+        public double RemainingBalance(float previousBalance, float principalPayment)
+        {
+            return Math.Round((previousBalance - principalPayment), 2);
+        }
+    }
+}
